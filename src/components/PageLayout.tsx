716beb8abd7e9f8644f0d7c6363bfe430f8bfeca
Ilya Gulya <ilya@gulya.me>
2025-08-12 19:04:49 +0500

    nits

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -9,7 +9,7 @@ interface PageLayoutProps {
 export function PageLayout({ children, className = "" }: PageLayoutProps) {
   return (
     <div className={`relative min-h-dvh bg-bg-beige ${className}`}>
-      <DecorLeft className="pointer-events-none fixed -left-8 top-16 w-40 opacity-60 z-0" />
+      <DecorLeft className="pointer-events-none fixed left-0 top-16 w-40 opacity-60 z-0" />
       <DecorRight className="pointer-events-none fixed -right-6 bottom-20 w-44 opacity-70 z-0" />
       <Bubbles className="pointer-events-none fixed left-1/3 top-8 w-24 opacity-50 z-0" />
       <div className="relative z-10">
